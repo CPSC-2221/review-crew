39e1737f323bcb01ce296aafbedd3ec8757d8675
Nikhil <jadhavnikhil825@gmail.com>
2024-10-26 20:04:20 -0700

    Review Tables , Review Character Limit
    Created Review Table
    Created Review Character limit Table

--- db/setup.go
+++ db/setup.go
@@ -18,3 +18,37 @@ func CreateUsersTable(c *gin.Context) {
 		})
 	}
 }
+
+func CreateReviewTable(c *gin.Context) {
+	_, err := dbpool.Exec(c,
+		"CREATE TABLE review ("+
+			"review_ID INT PRIMARY KEY"+
+			"email VARCHAR(255) NOT NULL,"+
+			"restaurant_ID INT NOT NULL,"+
+			"comment TEXT,"+
+			"date DATE,"+
+			"FOREIGN KEY(email) REFERENCES Users(email)"+
+			"FOREIGN KEY(restaurant_ID) REFRENCES Restaurant(restaurant_ID)"+");")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+}
+
+
+func CreateReviewCharacterLimitTable(c *gin.Context) {
+	_, err := dbpool.Exec(c,
+		"CREATE TABLE reviewCharacterLimit ("+
+			"restaurant_ID INT PRIMARY KEY"+
+			"character_limit INT NOT NULL,"+
+			"FOREIGN KEY(restaurant_ID) REFRENCE Review(restaurant_ID)"+
+			");")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+}
+
+
