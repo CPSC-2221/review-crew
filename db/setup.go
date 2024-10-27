faf85b5c6a50104ac28d42adbf005db7197dc3ec
Ethan Meier <e652@hotmail.ca>
2024-10-26 19:12:23 -0700

    push

--- db/setup.go
+++ db/setup.go
@@ -9,8 +9,8 @@ import (
 func CreateUsersTable(c *gin.Context) {
 	_, err := dbpool.Exec(c,
 		"CREATE TABLE users ("+
-			"username VARCHAR (50) UNIQUE NOT NULL,"+
 			"email VARCHAR (255) PRIMARY KEY"+
+			"username VARCHAR (50) UNIQUE NOT NULL,"+
 			");")
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
