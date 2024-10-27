ab54490a506a394c581129ea97a9465a52a956f3
Ethan Meier <e652@hotmail.ca>
2024-10-26 19:59:28 -0700

    create tables

--- db/setup.go
+++ db/setup.go
@@ -9,8 +9,50 @@ import (
 func CreateUsersTable(c *gin.Context) {
 	_, err := dbpool.Exec(c,
 		"CREATE TABLE users ("+
-			"email VARCHAR (255) PRIMARY KEY"+
-			"username VARCHAR (50) UNIQUE NOT NULL,"+
+			"email VARCHAR (255) PRIMARY KEY,"+
+			"username VARCHAR (50) UNIQUE NOT NULL"+
+			");")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+}
+
+func CreateRestaurantTable(c *gin.Context) {
+	_, err := dbpool.Exec(c,
+		"CREATE TABLE restaurant ("+
+			"restaurantID INTEGER PRIMARY KEY,"+
+			"name VARCHAR (255) NOT NULL,"+
+			"location VARCHAR (255) NOT NULL,"+
+			"description VARCHAR (255) NOT NULL,"+
+			"isPremium BOOLEAN NOT NULL"+
+			");")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+}
+
+func CreatePizzaImageTable(c *gin.Context) {
+	_, err := dbpool.Exec(c,
+		"CREATE TABLE hasPizzaImage ("+
+			"name VARCHAR (255) PRIMARY KEY,"+
+			"haPizzaImage BOOLEAN NOT NULL"+
+			");")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+}
+
+func CreateRepliesToTable(c *gin.Context) {
+	_, err := dbpool.Exec(c,
+		"CREATE TABLE RepliesTo ("+
+			"reviewIDRepliesTo INTEGER PRIMARY KEY REFERENCES review(reviewID),"+
+			"reviewIDIsRepliedTo INTEGER PRIMARY KEY REFERENCES review(reviewID)"+
 			");")
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
